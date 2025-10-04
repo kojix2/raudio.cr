160b78401c8a5e4968eae6454108dba4e1f066d9
kojix2 <2xijok@gmail.com>
2025-10-04 16:36:52 +0900

    CI: Remove ALSA dummy sound device setup and adjust audio device tests for CI environment

--- spec/raudio_spec.cr
+++ spec/raudio_spec.cr
@@ -6,25 +6,29 @@ describe Raudio do
   end
 
   describe Raudio::AudioDevice do
-    it "can initialize and close audio device" do
-      Raudio::AudioDevice.init
-      Raudio::AudioDevice.ready?.should be_true
-      Raudio::AudioDevice.close
-    end
-
-    it "can use block form for automatic initialization and cleanup" do
-      device_was_ready = false
-      Raudio::AudioDevice.open do
-        device_was_ready = Raudio::AudioDevice.ready?
+    if ENV["CI"]?
+      pending "Audio device tests are skipped in CI environment"
+    else
+      it "can initialize and close audio device" do
+        Raudio::AudioDevice.init
+        Raudio::AudioDevice.ready?.should be_true
+        Raudio::AudioDevice.close
       end
-      device_was_ready.should be_true
-    end
 
-    it "can set and get master volume" do
-      Raudio::AudioDevice.open do
-        Raudio::AudioDevice.master_volume = 0.5
-        volume = Raudio::AudioDevice.master_volume
-        volume.should be_close(0.5, 0.01)
+      it "can use block form for automatic initialization and cleanup" do
+        device_was_ready = false
+        Raudio::AudioDevice.open do
+          device_was_ready = Raudio::AudioDevice.ready?
+        end
+        device_was_ready.should be_true
+      end
+
+      it "can set and get master volume" do
+        Raudio::AudioDevice.open do
+          Raudio::AudioDevice.master_volume = 0.5
+          volume = Raudio::AudioDevice.master_volume
+          volume.should be_close(0.5, 0.01)
+        end
       end
     end
   end
