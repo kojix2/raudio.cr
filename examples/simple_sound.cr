646aff92c00ab0cfd3594bced81097b3fe887a45
kojix2 <2xijok@gmail.com>
2025-10-07 01:19:03 +0900

    Update simple_sound example to use embedded audio data

--- examples/simple_sound.cr
+++ examples/simple_sound.cr
@@ -1,34 +1,24 @@
 require "../src/raudio"
 
-# Simple sound playback example
-# Demonstrates basic sound loading and playback with automatic cleanup
+# Simple sound playback example with embedded audio data
+
+EMBED_TARGET_OGG = {{ read_file("ext/raudio/examples/resources/target.ogg") }}
+EMBED_WEIRD_WAV  = {{ read_file("ext/raudio/examples/resources/weird.wav") }}
 
 Raudio::AudioDevice.open do
-  puts "Audio device initialized"
-  puts "Master volume: #{Raudio::AudioDevice.master_volume}"
-
-  # Set master volume to 50%
   Raudio::AudioDevice.master_volume = 0.5
 
-  # Load and play a sound with automatic cleanup
-  begin
-    Raudio::Sound.load(
-      [File.expand_path("../ext/raudio/examples/resources/target.ogg", __DIR__),
-       File.expand_path("../ext/raudio/examples/resources/weird.wav", __DIR__)].sample
-    ) do |sound|
-      puts "Playing sound..."
-      sound.play
+  data, file_type = rand < 0.5 ? {EMBED_TARGET_OGG, ".ogg"} : {EMBED_WEIRD_WAV, ".wav"}
 
-      # Wait while sound is playing
-      while sound.playing?
-        sleep 10.milliseconds
-      end
+  wave = Raudio::Wave.load_from_memory(file_type, data.to_slice)
+  sound = Raudio::Sound.from_wave(wave)
+  wave.release
 
-      puts "Sound finished"
-    end
-  rescue ex : Raudio::SoundLoadError
-    puts "Error: #{ex.message}"
+  sound.play
+
+  while sound.playing?
+    sleep 10.milliseconds
   end
-end
 
-puts "Audio device closed"
+  sound.release
+end
